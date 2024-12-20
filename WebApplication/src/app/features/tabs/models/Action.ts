e15aa783a69c278352bbc2becb267d97fd8e3961
Aleksandar Živić <alziv808@gmail.com>
2024-12-20 01:04:56 +0100

    start of UI improvements

---
+++ WebApplication/src/app/features/tabs/models/Action.ts
@@ -0,0 +1,13 @@
+export interface Action {
+  id: number;
+  label: string;
+  icon: string;
+  action: ActionType;
+}
+
+export enum ActionType {
+  SAVE = 'SAVE',
+  UNDO = 'UNDO',
+  REDO = 'REDO',
+  TOGGLE_THEME = 'TOGGLE_THEME'
+}
