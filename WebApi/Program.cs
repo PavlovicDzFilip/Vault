8d150f10f2c75f397dfa29405531aad460d53f32
Pavlović Dž Filip <pavlovic.dz.filip@outlook.com>
2024-12-20 19:14:33 +0100

    Generate OpenApi json file on build

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers(options => options.Filters.Add<UnitOfWorkFilter>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 
 var app = builder.Build();
@@ -21,8 +21,7 @@ app.UseExceptionHandler();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
